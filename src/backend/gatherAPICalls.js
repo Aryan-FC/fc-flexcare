9acda468bbd67fc2ab6bb492d13827186202c633
Aryan3ROX <agaryan003@gmail.com>
2025-07-11 13:55:05 +0530

    added logs

--- src/backend/gatherAPICalls.js
+++ src/backend/gatherAPICalls.js
@@ -5,6 +5,9 @@ export async function setGuestlist(email, nickname) {
   const API_KEY = await secrets.getSecretValue("GATHER_API_KEY");
   const SPACE_ID = await secrets.getSecretValue("CURRENT_SPACE_ID");
 
+  console.log("spaceId as raw string:", SPACE_ID.value);
+  console.log("Type of spaceId:", typeof SPACE_ID.value);
+
   console.log("Sending to Gather:", {
     apiKey: API_KEY.value,
     spaceId: SPACE_ID.value,
@@ -15,16 +18,16 @@ export async function setGuestlist(email, nickname) {
   });
 
   try {
-  await axios.post("https://api.gather.town/api/setEmailGuestlist", {
-    apiKey: API_KEY.value,
-    spaceId: SPACE_ID.value,
-    guestlist: {
-      [email]: { name: nickname, affiliation: "ApprovedUser", role: "guest" },
-    },
-    overwrite: false,
-  });
-}
-catch (err) {
-  console.error("Axios error:", err.response?.data || err.message);
-}
+    await axios.post("https://api.gather.town/api/setEmailGuestlist", {
+      apiKey: API_KEY.value,
+      spaceId: SPACE_ID.value,
+      guestlist: {
+        [email]: { name: nickname, affiliation: "ApprovedUser", role: "guest" },
+      },
+      overwrite: false,
+    });
+  } catch (err) {
+    console.error("Axios error response:", err.response?.data);
+    console.error("Full error object:", err);
+  }
 }
