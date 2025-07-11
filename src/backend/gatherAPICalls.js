b8cc87fd4fc2d69d7f998fe7dffff4cf8a0e51c5
Aryan3ROX <agaryan003@gmail.com>
2025-07-11 13:47:52 +0530

    fixing backend v4

--- src/backend/gatherAPICalls.js
+++ src/backend/gatherAPICalls.js
@@ -1,14 +1,30 @@
-import axios from 'axios';
-import { secrets } from 'wix-secrets-backend.v2';
+import axios from "axios";
+import { secrets } from "wix-secrets-backend.v2";
 
 export async function setGuestlist(email, nickname) {
   const API_KEY = await secrets.getSecretValue("GATHER_API_KEY");
   const SPACE_ID = await secrets.getSecretValue("CURRENT_SPACE_ID");
 
+  console.log("Sending to Gather:", {
+    apiKey: API_KEY.value,
+    spaceId: SPACE_ID.value,
+    guestlist: {
+      [email]: { name: nickname, affiliation: "ApprovedUser", role: "guest" },
+    },
+    overwrite: false,
+  });
+
+  try {
   await axios.post("https://api.gather.town/api/setEmailGuestlist", {
     apiKey: API_KEY.value,
     spaceId: SPACE_ID.value,
-    guestlist: {[email]:{'name':nickname,'affiliation':'ApprovedUser','role':'guest'}},
-    overwrite: false
-  })
+    guestlist: {
+      [email]: { name: nickname, affiliation: "ApprovedUser", role: "guest" },
+    },
+    overwrite: false,
+  });
+}
+catch (err) {
+  console.error("Axios error:", err.response?.data || err.message);
+}
 }
